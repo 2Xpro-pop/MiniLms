84e8cc973dd148ca5cfef2e94b80f3b77d698c06
2Xpro-pop <bay.asanov@mail.ru>
2023-09-26 07:34:49 +0600

    Prepare for blocking

---
+++ Services/AccessBlockService.cs
@@ -0,0 +1,22 @@
+﻿using MiniLms.Models;
+
+namespace MiniLms.Services;
+
+public class AccessBlockService : IAccessBlockService
+{
+    private readonly IUserService _userService;
+    private readonly ICategoryService _categoryService;
+    private readonly ILessonsService _lessonsService;
+
+    public AccessBlockService(IUserService userService, ICategoryService categoryService, ILessonsService lessonsService)
+    {
+        _userService = userService;
+        _categoryService = categoryService;
+        _lessonsService = lessonsService;
+    }
+
+    public Task<IEnumerable<AccessBlock>> GetAccessBlockCategoriesForUser(Guid userId)
+    {
+    }
+    public Task<IEnumerable<AccessBlock>> GetAccessBlockLessonsForUser(Guid userId) => throw new NotImplementedException();
+}
---
+++ Services/IAccessBlockService.cs
@@ -0,0 +1,10 @@
+﻿using MiniLms.Models;
+
+namespace MiniLms.Services;
+
+public interface IAccessBlockService
+{
+    public Task Block(AccessBlock block);
+    public Task<IEnumerable<AccessBlock>> GetAccessBlockCategoriesForUser(Guid userId);
+    public Task<IEnumerable<AccessBlock>> GetAccessBlockLessonsForUser(Guid userId);
+}
