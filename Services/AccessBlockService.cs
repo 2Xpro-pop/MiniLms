df3bd5184ea67a7acc72e177836d23e59b8b8979
2Xpro-pop <bay.asanov@mail.ru>
2023-09-26 07:46:13 +0600

    implemented Interface IAccessBlockService and trying fixed workflow

--- Services/AccessBlockService.cs
+++ Services/AccessBlockService.cs
@@ -1,9 +1,11 @@
-﻿using MiniLms.Models;
+﻿using System.Collections.Generic;
+using MiniLms.Models;
 
 namespace MiniLms.Services;
 
 public class AccessBlockService : IAccessBlockService
 {
+    private readonly List<AccessBlock> _accessBlocks = new();
     private readonly IUserService _userService;
     private readonly ICategoryService _categoryService;
     private readonly ILessonsService _lessonsService;
@@ -15,8 +17,23 @@ public class AccessBlockService : IAccessBlockService
         _lessonsService = lessonsService;
     }
 
+    public Task Block(AccessBlock block)
+    {
+        _accessBlocks.Add(block);
+        return Task.CompletedTask;
+    }
     public Task<IEnumerable<AccessBlock>> GetAccessBlockCategoriesForUser(Guid userId)
     {
+        var filter = _accessBlocks.Where(ab => ab.UserId == userId)
+            .Where(ab => ab.CategoryId != null);
+
+        return Task.FromResult(filter);
     }
-    public Task<IEnumerable<AccessBlock>> GetAccessBlockLessonsForUser(Guid userId) => throw new NotImplementedException();
+    public Task<IEnumerable<AccessBlock>> GetAccessBlockLessonsForUser(Guid userId)
+    {
+        var filter = _accessBlocks.Where(ab => ab.UserId == userId)
+            .Where(ab => ab.LessonId != null);
+
+        return Task.FromResult(filter);
+    }
 }
